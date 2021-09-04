0570f9ce1e029188d3d8ce086934cf8c678b28f0
Fer Wicker <fer.wicker@gmail.com>
2021-09-04 10:27:01 +1000

    basic components for left side

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import React from "react";
+import RecipeList from "./components/RecipeList";
 
 function App() {
   return (
-    null
+    <RecipeList recipes={sampleRecipes} />
   );
 }
 
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions: `1. Put salt on Chicken/n
+      2. Put chicken in the oven/n
+      3. Eat the chicken`,
+      ingredients: [
+        {
+          id: 1,
+          name: 'Chicken',
+          amount: '2 pounds'
+        },
+        {
+          id: 2,
+          name: 'Salt',
+          amount: '1 tsp'
+        }
+      ]
+  },
+  {
+    id: 2,
+    name: 'Plain pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions: `1. Put paprika on pork/n
+      2. Put pork in the oven/n
+      3. Eat the pork`,
+      ingredients: [
+        {
+          id: 1,
+          name: 'Pork',
+          amount: '3 pounds'
+        },
+        {
+          id: 2,
+          name: 'Paprika',
+          amount: '2 tbsp'
+        }
+      ]
+  }
+]
 export default App;
